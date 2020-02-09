e975192e77ca6af33858be1b70585073d4d3a3b2
yunfei4swift <yunfei4cs@gmail.com>
2020-02-09 11:48:13 +0800

    update my demo-count project with vuex

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,41 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>我是从页面上直接获取的：{{ this.$store.state.count}}</h2>
+	<h2>我是从getters获取的计算后的值：{{ this.$store.getters.getStateCount}}</h2>
+	
+	<p>count的值：{{this.$store.state.count}}</p>
+	<button @click="addFunc">+</button>
+	<button @click="reductionFunc">-</button>
+	
+	<div style="border: 1px solid red;margin-top: 50px;">{{count1}}</div>
   </div>
 </template>
 
 <script>
+import {mapState,mapActions,mapGetters} from 'vuex';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  computed:mapState({
+	  count1:state=>state.count
+  }),
+  // computed:{
+	 //  mapState({
+		//   count1:state=>state.count
+	 //  })
+  // },
+  methods:{
+	addFunc(){
+		this.$store.dispatch("addFunc");
+		// this.$store.commit("add");
+	},
+	reductionFunc(){
+		this.$store.dispatch("reductionFunc");
+		// this.$store.commit("reduction");
+	}
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,33 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const store = new Vuex.Store({
+	state: {
+		count:1
+	},
+	getters: {
+		getStateCount:function(state){//上面的state
+			return state.count + 1
+		}
+	},
+	mutations: {
+		add(state){
+			state.count = state.count + 1
+		},
+		reduction(state){
+			state.count = state.count - 1
+		}
+	},
+	actions: {//注册actions类似vue里的methods
+		addFunc(context){//接收一个与store实例具有相同方法的属性的context对象
+			context.commit("add")
+		},
+		reductionFunc(context){//接收一个与store实例具有相同方法的属性的context对象
+			context.commit("reduction")
+		}
+	},
+	modules: {
+	}
 })
+
+export default store
